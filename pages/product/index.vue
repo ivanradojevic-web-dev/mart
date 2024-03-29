7ae2eb38eb94a5e37efd8ac9dac9801000040a7e
Ivan Radojevic <ivanr@Ivans-Mac-mini.local>
2024-03-29 17:30:11 +0100

    fetch products data via nitro server and made products page

---
+++ pages/product/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="bg-white">
+        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <header>
+                <h1 class="text-2xl font-bold tracking-tight text-gray-900">All Products</h1>
+            </header>
+
+            <!-- Display loading state -->
+            <div v-if="pending" class="text-center">
+                <p>Loading products...</p>
+            </div>
+
+            <!-- Display error state -->
+            <div v-if="error" class="text-center">
+                <p>An error occurred while fetching products.</p>
+            </div>
+
+            <!-- Display products once loaded -->
+            <section
+                v-if="!pending && !error"
+                class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+            >
+                <div v-for="(product, index) in products" :key="index" class="group relative">
+                    <div
+                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
+                    >
+                        <img
+                            src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                            :alt="product.description"
+                            class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                        />
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                        <div>
+                            <h3 class="text-sm text-gray-700">
+                                <a href="#">
+                                    <span aria-hidden="true" class="absolute inset-0" />
+                                    {{ product.title }}
+                                </a>
+                            </h3>
+                            <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+                        </div>
+                        <p class="text-sm font-medium text-gray-900">${{ product.price / 100 }}</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { data: products, error, pending } = await useFetch('/api/products')
+</script>
