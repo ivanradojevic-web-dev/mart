525bf624d169e31c80f7db4823eafe3e41c387f4
Ivan Radojevic <ivanr@Ivans-Mac-mini.local>
2024-04-05 19:20:28 +0200

    page properties

--- app.vue
+++ app.vue
@@ -4,10 +4,21 @@
 
 <script setup lang="ts">
 useHead({
-    title: 'My App',
-    meta: [{ name: 'description', content: 'My amazing site.' }],
+    title: 'Demo Ecommerce App',
+    meta: [{ name: 'Ecommerce App', content: 'Ecommerce products.' }],
     htmlAttrs: {
         lang: 'en'
     }
 })
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+    transition: all 0.16s;
+}
+.page-enter-from,
+.page-leave-to {
+    opacity: 0;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        pageTransition: { name: 'page', mode: 'out-in' }
+    },
     devtools: { enabled: true },
-    modules: ['@pinia/nuxt',
-    '@pinia-plugin-persistedstate/nuxt', 
-    '@nuxtjs/supabase', 
-    '@vee-validate/nuxt'
+    modules: [
+        '@pinia/nuxt',
+        '@pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/supabase',
+        '@vee-validate/nuxt'
     ],
     css: ['~/assets/css/main.css'],
     postcss: {
